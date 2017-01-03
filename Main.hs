f732cdf2a73a98f54ecc8c47c3620ee00a53f65d
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2017-01-03 19:33:01 +0300

    Rewrite Main to generate GraphViz code of friends

--- Main.hs
+++ Main.hs
@@ -1,3 +1,25 @@
-import Vk (getFriends, getUser, getFF)
+{-# LANGUAGE FlexibleInstances #-}
+import Control.Monad (forM)
+import System.Environment (getArgs)
 
-main = length <$> (getUser 210700286 >>= getFF 2) >>= print
+import Vk (getFriends, getUser, Profile(..))
+
+class ToDot a where
+  toDot :: a -> String
+
+instance ToDot Profile where
+  toDot p = (show . uid) p ++ " [ label = \"" ++ firstName p ++ " " ++ lastName p ++ "\" ];"
+
+instance ToDot ((,) Profile Profile) where
+  toDot (Profile _ _ uid, Profile _ _ uid') =
+    show uid ++ " -- " ++ show uid' ++ ";"
+
+main = do
+  target <- read <$> head <$> getArgs
+  fs <- getUser target >>= getFriends
+  putStrLn "graph {"
+  mapM (putStrLn . toDot) fs
+  forM fs $ \x -> do
+    adj <- filter (flip elem fs) <$> getFriends x
+    mapM (putStrLn . toDot . (,) x) adj
+  putStrLn "}"
